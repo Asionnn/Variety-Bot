18a1dc890d023543da23ee9e5f932de5c4b6c595
Collin Li <collin.li0499@gmail.com>
2018-07-14 01:21:06 -0400

    fixed messages

--- bot.js
+++ bot.js
@@ -6,15 +6,18 @@ bot.on('message', (message) => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!` 
     switch(message.content){
-	case "!ping":
-        message.reply('pong');
-	break;
 	case "!roll":
 		var num = Math.floor(Math.random() * 100)+1;
-		message.reply('rolled ' + num);
+		message.channel.send('rolled ' + num);
     break;
     case "!neel":
-        message.reply('your dad');
+        message.channel.send('your dad');
+    break;
+    case "!thinking":
+        message.react('🤔');
+    break;
+    case "!avatar":
+       message.channel.send('hey');
     break;
   
   
